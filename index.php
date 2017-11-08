e9b6c3eeb0ae4159c42137f9f0dc45ac35626014
Gunavaran <gunavaran.15@cse.mrt.ac.lk>
2017-11-08 22:40:34 +0530

    topbar

--- index.php
+++ index.php
@@ -3,59 +3,15 @@ include "connectDB.php";
 include "log_in/core.php";
 
 if (logged_in()){
+    include 'topbar.php';
 ?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-      <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CEB-Smart</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <link href="assets/css/custom.css" rel="stylesheet" />
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-</head>
-<body>
-    <div id="wrapper">
-        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Binary admin</a>
-            </div>
-  <div style="color: white;
-padding: 15px 50px 5px 50px;
-float: right;
-font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="log_in/logout.php" class="btn btn-danger square-btn-adjust">Logout</a> </div>
-        </nav>
-           <!-- /. NAV TOP  -->
-                <nav class="navbar-default navbar-side" role="navigation">
-            <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-				<li class="text-center">
-                    <img src="assets/img/find_user.png" class="user-image img-responsive"/>
-					</li>
-
-                    <?php
-                    include "sidebar.php";
-                    ?>
-
-                </ul>
-
-            </div>
-
-        </nav>
         <!-- /. NAV SIDE  -->
         <div id="page-wrapper" >
             <div id="page-inner">
                 <div class="row">
                     <div class="col-md-12">
                      <h2>Blank Page</h2>
-                        <h5>Welcome <?php echo $_SESSION['user_id'] ?> , Love to see you back. </h5>
+                        <h5>Welcome <?php echo getUserDetail('username') ?> , Love to see you back. </h5>
 
                     </div>
                 </div>
--- log_in/core.php
+++ log_in/core.php
@@ -19,9 +19,17 @@ function logged_in(){
 }
 
 function getUserDetail($data){
-    require '../connectDB.php';
-    $username= $_SESSION['username'];
-    $query_user_detail = "SELECT $data FROM customer WHERE  user_id='$username'";
+    require 'connectDB.php';
+    $user_id= $_SESSION['user_id'];
+
+    if ($_SESSION['user_type'] == 'customer'){
+        $query_user_detail = "SELECT $data FROM customer WHERE  user_id='$user_id'";
+    } else if ($_SESSION['user_type'] == 'admin'){
+        $query_user_detail = "SELECT $data FROM staff natural join admin WHERE user_id='$user_id'";
+    } else if ($_SESSION['user_type'] == 'meter_reader') {
+        $query_user_detail = "SELECT $data FROM staff natural join meter_reader WHERE user_id='$user_id'";
+    }
+
     if ($query_user_run = mysqli_query($link,$query_user_detail)){
         $user_detail_row = mysqli_fetch_assoc($query_user_run);
         return $user_detail_row[$data];
