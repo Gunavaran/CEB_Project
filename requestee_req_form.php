bae1567e97a1154e6feec1a18863526a827cba41
Gunavaran <gunavaran.15@cse.mrt.ac.lk>
2017-12-18 08:10:49 +0530

    Final Day - view corrected

--- requestee_req_form.php
+++ requestee_req_form.php
@@ -60,7 +60,7 @@ if (!logged_in()){
             </div>
             <input type="submit" value="Request" class="btn btn-primary" style="margin-right: 10px; float: right">
         </form>
-        <a style="float: right; margin-right: 20px; margin-top:" href="log_in/log_in_page.php" class="btn btn-success" >Back</a>
+        <a style="float: right; margin-right: 20px; margin-top:" href="index.php" class="btn btn-success" >Back</a>
         <hr />
         <?php
         if (isset($_POST['connection_type']) && isset($_POST['resident_no']) &&
